c1a148bb52722abaf426beb1ee8ceeae0f59ba10
Alessandro Sica <alessandrosica500@gmail.com>
2025-06-17 16:01:22 +0100

    Initial SetUp

---
+++ Check_Set_Up.py
@@ -0,0 +1,5 @@
+import torch, transformers, cv2, wandb
+print("Torch:", torch.__version__)
+print("Transformers:", transformers.__version__)
+print("OpenCV:", cv2.__version__)
+print("W&B:", wandb.__version__)
