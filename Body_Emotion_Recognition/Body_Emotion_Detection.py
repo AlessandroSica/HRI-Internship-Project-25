c473bb39713b4d5469ce2e278e902ed368defbc9
Alessandro Sica <alessandrosica500@gmail.com>
2025-06-25 19:27:21 +0100

    The body emotion recognition is now working

---
+++ Body_Emotion_Recognition/Body_Emotion_Detection.py
@@ -0,0 +1,162 @@
+import mediapipe as mp # Face detection library, MediaPipe Holistic module provides a holistic approach to human pose, face, and hand detection. Whenever you want to use MediaPipe, you need to import the mediapipe library first. 
+# Whenever you then call the variable mp, it refers to the mediapipe library, which is a collection of tools and models for computer vision tasks, including face detection, pose estimation, and hand tracking.
+import cv2 # OpenCV library for computer vision tasks, such as image processing and video capture.
+import csv # This module is used to write data to a CSV file.
+import os # This module is used to interact with the operating system, such as creating directories and checking if files exist.
+import numpy as np # NumPy is used for numerical operations, such as creating arrays and manipulating data.
+import pandas as pd # Importing pandas for data manipulation, to analyze or CSV file.
+import pickle # Importing pickle to save the trained models to disk for later use.  
+
+with open('fit_models.pkl', 'rb') as f:  # Opening the file 'fit_models.pkl' in read-binary mode to load the trained models.
+    model = pickle.load(f)  # Loading the trained model from the file.
+
+mp_drawing = mp.solutions.drawing_utils # Drawing utilities for visualizing landmarks and connections.
+mp_holistic = mp.solutions.holistic # Holistic module for detecting face, pose, and hand landmarks. Imported from MediaPipe library.
+
+cap = cv2.VideoCapture(1) # Open the default camera (0) for video capture.
+
+# Initialize the MediaPipe Holistic model with default parameters.
+with mp_holistic.Holistic(
+    min_detection_confidence=0.5, # Minimum confidence threshold for face detection.
+    min_tracking_confidence=0.5 # Minimum confidence threshold for landmark tracking.
+) as holistic: # Use the holistic model in a context manager to ensure proper resource management.
+    
+    # Creating the CSV file to store the landmark data.
+    # In order to do it we need to do the same steps that we do inside the loop once, so to initialize it. As we can't put it into the loop or otherwise it will create a new file every time the loop runs.
+    #----------------------------------------------------------------------------------------------------
+    ret, frame = cap.read() # Read a frame from the camera to initialize the model.
+    if not ret: # Check if the frame was successfully captured.
+        print("Failed to grab initial frame.")
+        cap.release()
+        cv2.destroyAllWindows()
+        exit()
+
+    image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB) # Convert the frame to RGB format for MediaPipe processing.
+    image.flags.writeable = False # Set the image to non-writeable for faster processing.
+    results = holistic.process(image) # Make detections and track landmarks in the image.
+    image.flags.writeable = True
+
+    if results.face_landmarks and results.pose_landmarks:  # Check if both face and pose landmarks are detected.
+        num_coords = len(results.face_landmarks.landmark) + len(results.pose_landmarks.landmark) # Get the total number of landmarks detected for both face and pose.
+        landmark = ['class'] # Initialize a list to store landmark names, starting with 'class' for the name of the column where all the emotion classes will be stored, such as 'happy', 'sad', etc.
+        for val in range(1, num_coords + 1): # Create a list of landmark names for each coordinate (x, y, z) for each landmark, and one for visibility (v) of each landmark.
+            landmark += ['x{}'.format(val), 'y{}'.format(val), 'z{}'.format(val), 'v{}'.format(val)]
+
+        if not os.path.exists('coords.csv'): # Check if the CSV file already exists.
+            with open('coords.csv', mode='w', newline='') as f: # If it doesn't exist, create it and write the header row.
+                csv_writer = csv.writer(f, delimiter=';', quotechar='"', quoting=csv.QUOTE_MINIMAL) # Write the header row to the CSV file.
+                csv_writer.writerow(landmark) # This will create the CSV file with the header row containing the names of the columns: 'class', 'x1', 'y1
+    #----------------------------------------------------------------------------------------------------
+
+    while cap.isOpened(): # Loop until the camera is closed.
+        ret, frame = cap.read() # Read a frame from the camera.
+        if not ret:
+            print("Failed to grab frame.")
+            break
+
+        # Convert the frame to RGB format for MediaPipe processing.
+        # When using MediaPipe, the input image should be in RGB format.
+        # OpenCV captures images in BGR format, so we need to convert it.
+        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        image.flags.writeable = False # Set the image to non-writeable for faster processing.
+        # Make detections and track landmarks in the image.
+        results = holistic.process(image)
+        image.flags.writeable = True # Set the image back to writeable after processing.
+        
+        # Convert the image back to BGR format for OpenCV display.
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+        # Draw face landmarks on the image.
+        
+        mp_drawing.draw_landmarks(
+            image, results.face_landmarks, mp.solutions.face_mesh.FACEMESH_TESSELATION, # Draw face landmarks using the FACEMESH_TESSELATION connections.
+            # The FACEMESH_TESSELATION is a predefined set of connections for face landmarks
+            # mp_holistic.FACE_CONNECTIONS wasn't working, so I used mp.solutions.face_mesh.FACEMESH_TESSELATION instead.
+            landmark_drawing_spec=mp_drawing.DrawingSpec(color=(200, 0, 0), thickness=1, circle_radius=1), # Color of the dots (landmarks). It's BGR format, so (0, 255, 0) is green.
+            connection_drawing_spec=mp_drawing.DrawingSpec(color=(0, 255, 0), thickness=1, circle_radius=1) # Color of the lines (connections between landmarks).
+        ) 
+        
+        # Draw pose landmarks on the image.
+        mp_drawing.draw_landmarks(
+            image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS,
+            landmark_drawing_spec=mp_drawing.DrawingSpec(color=(245, 117, 66), thickness=2, circle_radius=2),
+            connection_drawing_spec=mp_drawing.DrawingSpec(color=(245, 66, 230), thickness=2, circle_radius=2)
+        )
+
+        # Draw left hand landmarks on the image.
+        mp_drawing.draw_landmarks(
+            image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS,
+            landmark_drawing_spec=mp_drawing.DrawingSpec(color=(121, 22, 76), thickness=2, circle_radius=3),
+            connection_drawing_spec=mp_drawing.DrawingSpec(color=(121, 44, 250), thickness=2, circle_radius=2)
+        )
+
+        # Draw right hand landmarks on the image.
+        mp_drawing.draw_landmarks(
+            image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS,
+            landmark_drawing_spec=mp_drawing.DrawingSpec(color=(80, 22, 10), thickness=2, circle_radius=3),
+            connection_drawing_spec=mp_drawing.DrawingSpec(color=(80, 44, 121), thickness=2, circle_radius=2)
+        )
+
+        num_coords = len(results.face_landmarks.landmark) + len(results.pose_landmarks.landmark)
+        # num_coords stores the total number of landmarks detected for both face and pose, not including hands
+        
+        # Creating the CSV file to store the landmark data.
+
+        landmark = ['class'] # Initialize a list to store landmark names, starting with 'class' for the name of the column where all the emotion classes will be stored, such as 'happy', 'sad', etc.
+        # This CSV file will be used to label the data for training a machine learning model later.
+        
+        for val in range(1, num_coords+1):
+            #landmark += [f'x{val}', f'y{val}', f'z{val}', f'v{val}'] # equivalent to the following line, but using f-strings for better readability.
+            landmark += ['x{}'.format(val), 'y{}'.format(val), 'z{}'.format(val), 'v{}'.format(val)] # Create a list of landmark names for each coordinate (x, y, z) for each landmark, and one for visibility (v) of each landmark.
+            # The visibility (v) indicates whether the landmark is visible in the frame (1) or not (0).
+            # Note that the visibility of all the landmarks of the face is 0 by default for some reason.
+            # This list stores all of the names of the columns of CSV file: 'x1', 'y1'... One for every coordinate to see how they vary overtime
+
+        class_name = 'Very Happy' # Define the class name for the current emotion, this will be used to label the data in the CSV file.
+        # You can change this to any emotion class you want to label the data with, such as 'sad', 'angry', etc.
+
+        # Export landmarks to CSV file.
+        try:
+            # Extract pose landmarks.
+            pose = results.pose_landmarks.landmark if results.face_landmarks else [] # Extract pose landmarks from the results.
+            pose_row = list(np.array([[landmark.x, landmark.y, landmark.z, landmark.visibility] for landmark in pose]).flatten())
+            # Put all the pose landmarks coordinates into a single row array, without any other information, for every landmark in pose.
+            # Flatten the array to make it a single row, where each landmark's x, y, z coordinates and visibility are stored sequentially.
+            # Flatten collapses an array of any shape into a one-dimensional array.
+
+            # Extract face landmarks.
+            face = results.face_landmarks.landmark if results.face_landmarks else [] # Extract face landmarks from the results, if they exist.
+            face_row = list(np.array([[landmark.x, landmark.y, landmark.z, landmark.visibility] for landmark in face]).flatten())
+
+            row = pose_row + face_row # Combine pose and face landmarks into a single row.
+
+            # Make detections.
+            x = pd.DataFrame([row]) # Create a DataFrame using pandas from the row of landmarks, which contains the coordinates and visibility of each landmark.
+            body_language_class = model.predict(x)[0] # Predict the body language class using the trained model. Predict, as in the model will predict the emotion class based on the landmarks detected in the current frame.
+            # 0 is the index of the predicted class, as the classes are ordered based on their accuracy given the input, so the one with index 0 is the most accurate prediction.
+            body_language_prob = model.predict_proba(x)[0] # Get the probabilities of class with index 0 for the current frame.
+            print(body_language_class, body_language_prob) # Print the predicted class and its probability.
+
+            if results.pose_landmarks and results.face_landmarks: # Check if both pose and face landmarks are detected.
+                try:
+                # Grab ear coordinate (x, y), in order to use it as a reference on where to print the text on the frame to show the predicted emotion.
+                    left_ear = results.pose_landmarks.landmark[mp_holistic.PoseLandmark.LEFT_EAR] # Get the coordinates of the left ear landmark, from the data gathered by the holistic model and stored in results.
+                    coords = tuple(np.multiply( # OpenCV expects a tuple of coordinates in the format (x, y) for drawing text on the frame.
+                        np.array((left_ear.x, left_ear.y)),
+                        [640, 480]  # Multiply the coordinates by the width and height of the frame, using numpy, to get the actual pixel values.
+                    ).astype(int)) # It has to be converted to integers, as the coordinates are in float format.
+            
+                    cv2.rectangle(image, (coords[0], coords[1] + 5), (coords[0] + len(body_language_class) * 20, coords[1] - 20), (245, 117, 16), -1) # Draw a rectangle around the left ear landmark to highlight it.
+                    cv2.putText(image, body_language_class, coords, cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA) # Draw the text reading the predicted emotion class on the frame at the coordinates of the left ear landmark.
+                except Exception as e:
+                    print("Drawing failed:", e) # Handle any exceptions that may occur during the drawing process.
+        
+        except:
+            pass # Handle any exceptions that may occur during the export process.
+
+        cv2.imshow('Holistic Model Detection', image) # Display the original frame in a window.
+
+        if cv2.waitKey(10) & 0xFF == ord('q'): # If the 'q' key is pressed, break the loop and exit.
+            break   
+
+cap.release() # Release the camera resource.
+cv2.destroyAllWindows() # Close all OpenCV windows.
--- Body_Emotion_Recognition/Training_Body_Emotion_Recognition.py
+++ Body_Emotion_Recognition/Training_Body_Emotion_Recognition.py
@@ -10,6 +10,10 @@ from sklearn.linear_model import LogisticRegression, RidgeClassifier # Importing
 from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier # Importing RandomForestClassifier and GradientBoostingClassifier for ensemble learning methods.
 # All four machine learning classifiers are used for classification tasks, where the goal is to predict a categorical label based on input features.
 
+from tqdm import tqdm
+import time 
+# Importing tqdm for progress bars and time for time-related functions.
+
 df = pd.read_csv('coords.csv', delimiter=';') # Creating an empty DataFrame to store the landmark data. 
 # The DataFrame will be used to store the coordinates of the landmarks detected by MediaPipe Holistic.
 # We do this so we can work easily with the data using pandas, a powerful data manipulation library in Python. 
@@ -25,20 +29,33 @@ y = df['class'] # Extracting the 'class' column from the DataFrame to create the
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=1234) # Splitting the dataset into training and testing sets.
 # x_train and y_train will be used to train the model, while x_test and y_test will be used to evaluate its performance.
 # The set is split into 70% training data and 30% testing data, with a random state for reproducibility.    
-# Random state ensures that the split is reproducible, meaning that every time you run the code, you will get similar split of data.
+# Random is used to shuffle the data before splitting, ensuring that the training and testing sets are representative of the overall dataset.
 
+'''
 pipelines = { # Creating a dictionary to store different machine learning pipelines.
     'lr': make_pipeline(StandardScaler(), LogisticRegression()), # Logistic Regression pipeline with feature scaling.
     'rc': make_pipeline(StandardScaler(), RidgeClassifier()), # Ridge Classifier pipeline with feature scaling.
     'rf': make_pipeline(StandardScaler(), RandomForestClassifier()), # Random Forest Classifier pipeline with feature scaling.
     'gb': make_pipeline(StandardScaler(), GradientBoostingClassifier()) # Gradient Boosting Classifier pipeline with feature scaling.
 }
+'''
+from sklearn.ensemble import HistGradientBoostingClassifier  # faster alternative
 
+pipelines = {
+    'lr': make_pipeline(StandardScaler(), LogisticRegression(max_iter=10000)),
+    'rc': make_pipeline(StandardScaler(), RidgeClassifier()),
+    'rf': make_pipeline(StandardScaler(), RandomForestClassifier(n_estimators=100, max_depth=5)),
+    'gb': make_pipeline(StandardScaler(), HistGradientBoostingClassifier())  # much faster!
+}
+
+LogisticRegression(max_iter=10000) # Setting the maximum number of iterations for the Logistic Regression model to 1000.
+
+'''
 fit_models = {} # Creating an empty dictionary to store the fitted (trained) models.
 for algo, pipeline in pipelines.items(): # Looping through the pipelines dictionary (created above, which contains 4 different models) to fit each model.
     model = pipeline.fit(x_train, y_train) # Fitting the pipeline to the training data, or training each of the 4 models to the training data (x_train and y_train).
     fit_models[algo] = model # Storing the fitted (trained) model in the fit_models dictionary with the algorithm name as the key, (alo for algorithm).
-
+'''
 # First, we create an empty dictionary called 'fit_models'. This dictionary will be used to store all the models after they are trained (also known as "fitted").
 # We then loop through each item in the 'pipelines' dictionary. Each item in this dictionary represents a machine learning algorithm (like Random Forest, SVM, etc.)
 # and its corresponding pipeline. A pipeline is a sequence of steps that includes data preprocessing (like scaling or transforming the data)
@@ -48,3 +65,35 @@ for algo, pipeline in pipelines.items(): # Looping through the pipelines diction
 # After the training is done, we store the resulting trained model in the 'fit_models' dictionary.
 # We use the name of the algorithm (e.g., "Random Forest", "Logistic Regression") as the key, and the trained model as the value.
 # This allows us to keep track of all the different models we trained and easily access or compare them later.
+
+fit_models = {} # Creating an empty dictionary to store the fitted (trained) models.
+
+model_names = {
+    'lr': 'Logistic Regression',
+    'rc': 'Ridge Classifier',
+    'rf': 'Random Forest',
+    'gb': 'Gradient Boosting'
+}
+
+for algo in tqdm(pipelines, desc="Training models", unit="model"): # Looping through the pipelines dictionary to train each model.
+    print(f"🔄 Training {model_names[algo]}...") # Printing the loading bar in the output
+    start = time.time() # Starting the timer to measure the training time of each model.
+    model = pipelines[algo].fit(x_train, y_train) # Fitting the pipeline to the training data, or training each of the 4 models to the training data (x_train and y_train).
+    end = time.time() # Ending the timer to measure the training time of each model.
+    print(f"✅ {model_names[algo]} trained in {end - start:.2f} seconds.\n") # Printing the training time of each model.
+    fit_models[algo] = model # Storing the fitted (trained) model in the fit_models dictionary with the algorithm name as the key, (alo for algorithm).
+
+# This code trains multiple machine learning models using a loop.
+# The aim is to then evaluate their performance on a test dataset.
+# And see which model performs best for the task at hand.
+
+from sklearn.metrics import accuracy_score # Importing accuracy_score from sklearn.metrics to evaluate the performance of the models.
+import pickle # Importing pickle to save the trained models to disk for later use.  
+
+for algo, model in fit_models.items():
+    yhat = model.predict(x_test) # Making predictions on the test set using the trained model.
+    print(algo, accuracy_score(y_test, yhat)) # Printing the algorithm name and its accuracy score on the test set.
+
+with open('fit_models.pkl', 'wb') as f: # Saving the best trained models to a file using pickle.
+# wb stands for "write binary", which is used to write binary files.
+    pickle.dump(fit_models['rf'], f) # Saving the Random Forest model to a file named 'fit_models.pkl'.
